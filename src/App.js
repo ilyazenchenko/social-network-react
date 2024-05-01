3b3f6ef8c7e8ac3986491b5a27d1eb4b0fd66d5c
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2024-05-01 20:54:25 +0300

    проверка аутентификации пользователя до рендерингка

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginForm from './components/auth/LoginForm.js';
 import RegisterForm from './components/auth/RegisterForm.js';
 import UserProfile from './components/UserProfile.js';
-import NewsPage from './components/NewsPage.js'; // Импортируйте NewsPage
+import NewsPage from './components/NewsPage.js';
+import SearchPage from './components/SearchPage.js';
 import axios from 'axios';
 import { AuthUserIdProvider } from './AuthUserIdContext';
-import SearchPage from './components/SearchPage.js';
 
 axios.defaults.withCredentials = true;
 
 function App() {
   const [user, setUser] = useState(null);
+  const [isAuthenticating, setIsAuthenticating] = useState(true); // Состояние загрузки для проверки аутентификации
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
         const response = await axios.get('http://localhost:8080/auth/user', { withCredentials: true });
         setUser(response.data);
-        console.log("auth user", response.data)
       } catch {
         setUser(null);
+      } finally {
+        setIsAuthenticating(false); // Устанавливаем, что аутентификация завершена
       }
     };
     checkAuth();
   }, []);
 
+  if (isAuthenticating) {
+    return <div>Loading...</div>; // Или другой индикатор загрузки
+  }
+
   return (
     <AuthUserIdProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={!user ? <LoginForm onLoginSuccess={setUser} /> : <Navigate replace to="/" />} />
-          <Route path="/register" element={!user ? <RegisterForm onRegisterSuccess={setUser} /> : <Navigate replace to="/" />} />
-          <Route path="/:id" element={<UserProfile onLogout={() => setUser(null)} />} />
-          <Route path="/news" element={!user ? <Navigate replace to="/" /> : <NewsPage />} />
-          <Route path="/search" element={!user ? <Navigate replace to="/" /> : <SearchPage />} />
-          <Route path="/" element={user ? <Navigate replace to={`/${user.id}`} /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={!user ? <LoginForm onLoginSuccess={setUser} /> : <Navigate replace to={`/${user?.id}`} />} />
+          <Route path="/register" element={!user ? <RegisterForm onRegisterSuccess={setUser} /> : <Navigate replace to={`/${user?.id}`} />} />
+          <Route path="/:id" element={user ? <UserProfile onLogout={() => setUser(null)} /> : <Navigate replace to="/login" />} />
+          <Route path="/news" element={user ? <NewsPage /> : <Navigate replace to="/login" />} />
+          <Route path="/search" element={user ? <SearchPage /> : <Navigate replace to="/login" />} />
+          <Route path="/" element={!user ? <Navigate replace to="/login" /> : <Navigate replace to={`/${user?.id}`} />} />
         </Routes>
       </Router>
     </AuthUserIdProvider>
